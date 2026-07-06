8b34cfac468321ffbba406e90ffa3be97b3510c9
sariyan-0 <sariyan-0@users.noreply.github.com>
2026-07-06 05:10:57 -0300

    fix: proxy admin routes in nginx vhost

--- install.sh
+++ install.sh
@@ -395,6 +395,15 @@ server {
         proxy_set_header   X-Real-IP \$remote_addr;
         proxy_read_timeout 15s;
     }
+    location ^~ /admin {
+        proxy_pass         http://127.0.0.1:${wh_port};
+        proxy_http_version 1.1;
+        proxy_set_header   Host \$host;
+        proxy_set_header   X-Real-IP \$remote_addr;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto \$scheme;
+        proxy_read_timeout 15s;
+    }
     location /health {
         proxy_pass http://127.0.0.1:${wh_port}/health;
     }
